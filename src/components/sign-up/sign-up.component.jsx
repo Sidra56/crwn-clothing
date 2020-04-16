bc2b1a66692e04cfea85ec8b07c4dcdb671c4977
sidra56 <sidraabdullah56@gmail.com>
2020-04-16 21:19:13 +0500

    third

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './sign-up.style.scss';
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
