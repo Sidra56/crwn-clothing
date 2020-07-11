742a24b7057b470e85da7bb90aa1e46c09a8497f
sidra56 <sidraabdullah56@gmail.com>
2020-07-11 17:42:23 +0500

    Updates

---
+++ precache-manifest.1394defca0b593023526e14d5ea9cb4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b4afa0ccbb3bf27e833f67ba5cf76c9",
+    "url": "/CompanyPortfolio/index.html"
+  },
+  {
+    "revision": "a3d246d00e631a1bc218",
+    "url": "/CompanyPortfolio/static/css/main.cbdffeca.chunk.css"
+  },
+  {
+    "revision": "7b35d03e788cada8cacc",
+    "url": "/CompanyPortfolio/static/js/2.8f82fe58.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/CompanyPortfolio/static/js/2.8f82fe58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3d246d00e631a1bc218",
+    "url": "/CompanyPortfolio/static/js/main.7676d000.chunk.js"
+  },
+  {
+    "revision": "25d637a6128a9c27b8bd",
+    "url": "/CompanyPortfolio/static/js/runtime-main.526a637f.js"
+  },
+  {
+    "revision": "7943cd2ac935c1becbcd69c1ddeafbcb",
+    "url": "/CompanyPortfolio/static/media/crown.7943cd2a.svg"
+  }
+]);
